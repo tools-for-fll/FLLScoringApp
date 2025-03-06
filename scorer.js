0ef6181e96cbbb8ec88526bd7e1ce543ec9cbf64
Brian Kircher <36779728+bckircher@users.noreply.github.com>
2025-03-06 15:32:26 -0600

    Another attempt at an iOS spash screen.

--- scorer.js
+++ scorer.js
@@ -23,6 +23,90 @@ let panelAnimation = false;
 // scoresheets).
 let readonly = false;
 
+// Adapted from ios-pwa-splash <https://github.com/avadhesh18/iosPWASplash>
+function
+iosPWASplash(icon, color = "white")
+{
+  // Check if the provided 'icon' is a valid URL
+  if((typeof icon !== "string") || (icon.length === 0))
+  {
+    throw new Error("Invalid icon URL provided");
+  }
+
+  // Calculate the device's width and height
+  const deviceWidth = screen.width;
+  const deviceHeight = screen.height;
+
+  // Calculate the pixel ratio
+  const pixelRatio = window.devicePixelRatio || 1;
+
+  console.log(`width: ${deviceWidth} height: ${deviceHeight} ratio: ${pixelRatio}`);
+
+  // Create two canvases and get their contexts to draw landscape and portrait
+  // splash screens.
+  const canvas = document.createElement("canvas");
+  const canvas2 = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  const ctx2 = canvas2.getContext("2d");
+
+  // Create an image element for the icon
+  const iconImage = new Image();
+
+  iconImage.onerror = function ()
+  {
+    throw new Error("Failed to load icon image");
+  };
+
+  iconImage.src = icon;
+
+  // Load the icon image.
+  iconImage.onload = function ()
+  {
+    // Calculate the icon size based on the device's screen size.
+    const min = Math.min(deviceWidth, deviceHeight) * pixelRatio;
+    const iconSizew = (min * 3) / 5;
+    const iconSizeh = (min * 3) / 5;
+
+    canvas.width = deviceWidth * pixelRatio;
+    canvas2.height = canvas.width;
+    canvas.height = deviceHeight * pixelRatio;
+    canvas2.width = canvas.height;
+    ctx.fillStyle = color;
+    ctx2.fillStyle = color;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
+
+    // Calculate the position to center the icon
+    const x = (canvas.width - iconSizew) / 2;
+    const y = (canvas.height - iconSizeh) / 2;
+    const x2 = (canvas2.width - iconSizew) / 2;
+    const y2 = (canvas2.height - iconSizeh) / 2;
+
+    // Draw the icon with the calculated size
+    ctx.drawImage(iconImage, x, y, iconSizew, iconSizeh);
+    ctx2.drawImage(iconImage, x2, y2, iconSizew, iconSizeh);
+    const imageDataURL = canvas.toDataURL("image/png");
+    const imageDataURL2 = canvas2.toDataURL("image/png");
+
+    // Create the first startup image <link> tag (splash screen)
+    const link = document.createElement("link");
+    link.setAttribute("rel", "apple-touch-startup-image");
+    link.setAttribute("media", "screen and (orientation: portrait)");
+    link.setAttribute("href", imageDataURL);
+    document.head.appendChild(link);
+
+    // Create the second startup image <link> tag (splash screen)
+    const link2 = document.createElement("link");
+    link2.setAttribute("rel", "apple-touch-startup-image");
+    link2.setAttribute("media", "screen and (orientation: landscape)");
+    link2.setAttribute("href", imageDataURL2);
+    document.head.appendChild(link2);
+  };
+}
+
+// Generate iOS splash screens from the favicon.
+iosPWASplash("favicon.png", "#000000");
+
 // Extend JQuery by adding a showModal() method (mimic-ing the corresponding
 // method in the standard DOM model).
 $.fn.extend({showModal: function()
